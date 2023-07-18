fad0f661cc6f5c7d95575322665280484779b999
NickAMars <remember.marsman@gmail.com>
2023-07-18 12:31:58 -0400

    fixing issue with text and regular calls

--- server.js
+++ server.js
@@ -32,48 +32,55 @@ const service = function(routes) {
 		let chosenHandler = typeof(server.router[route]) !== 'undefined'? server.router[route].bind(routes) : routes.notFound;
 		let decoder = new StringDecoder('utf-8'); // use to convert buffer to string
 		let buffer = '';
+		let data = {
+			'path': trimmedPath, // the pathname
+			// Take it as a string 
+			method, // [ get, post , put, delete ]
+			headers,
+			params,
+			query, // objects after the path name
+		};
+		const callback = function(statusCode, payload){ // the callback comes from the router
+			// check if its a number
+			statusCode = typeof(statusCode) == 'number' ? statusCode : STATUS_CODE.OK;
+			// check if its an object
+			payload = typeof(payload) == 'object'? payload : null;
+			// json to string
+			let payloadString = payload?  JSON.stringify(payload): null;
+			//parse this as if it was json
+			res.setHeader('Content-Type', 'application/json');
+			res.writeHead(statusCode);
+			if(payloadString){
+				res.end(payloadString);
+			}else{
+				res.end();
+			}
+		};
+		//THIS IS ONLY FOR THE BODY
 		//receives an body
-		req.on(STREAM.DATA, function(data){
-			// decode buffer object into a string
-			buffer += decoder.write(data);
-		});
-		req.on(STREAM.END, function(){
-
-			buffer += decoder.end();
-			let data = {
-				'path': trimmedPath, // the pathname
-				// Take it as a string 
-				//  helpers.parseJsonToObject(buffer)
-				'body': buffer,
-				method, // [ get, post , put, delete ]
-				headers,
-				params,
-				query, // objects after the path name
-			};
-
-			const callback = function(statusCode, payload){ // the callback comes from the router
-				// check if its a number
-				statusCode = typeof(statusCode) == 'number' ? statusCode : STATUS_CODE.OK;
-				// check if its an object
-				payload = typeof(payload) == 'object'? payload : null;
-				// json to string
-				let payloadString = payload?  JSON.stringify(payload): null;
-				//parse this as if it was json
-				res.setHeader('Content-Type', 'application/json');
-				res.writeHead(statusCode);
-				if(payloadString){
-					res.end(payloadString);
+		if(headers['content-type']){
+			req.on(STREAM.DATA, function(data){
+				// decode buffer object into a string
+				buffer += decoder.write(data);
+			});
+			req.on(STREAM.END, function(){
+				buffer += decoder.end();
+				data.body = buffer;
+			// 	// base on the route that was chosen
+				if(chosenHandler){
+					chosenHandler(data, callback);
 				}else{
-					res.end();
+					callback(STATUS_CODE.BAD_REQUEST, {[ERROR_MESSAGE.ERROR] : ERROR_MESSAGE.NOT_FOUND}) 
 				}
-			};
-		// 	// base on the route that was chosen
+			});
+		}else{
+			// without text 
 			if(chosenHandler){
 				chosenHandler(data, callback);
 			}else{
 				callback(STATUS_CODE.BAD_REQUEST, {[ERROR_MESSAGE.ERROR] : ERROR_MESSAGE.NOT_FOUND}) 
 			}
-		});
+		}
 	};
 	//Create Server
 	server.httpServer = http.createServer((req, res) =>{
